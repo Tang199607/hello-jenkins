897f7781bc4804a8442ad075043f11c209e94301
Tang199607 <909219477@qq.com>
2023-03-12 11:28:41 +0800

    初始化项目

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author TangYong
+ * @date 2023年03月12日 11:09
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
